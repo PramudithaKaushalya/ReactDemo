0e138579a684684ad91b057c0d98bec09c30a625
Pramuditha Kaushalya <pramuditha.kaushalya@gmail.com>
2019-09-09 16:12:41 +0530

    Filter

--- src/container/Dashboard.js
+++ src/container/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
-import ScrollPad from 'react-scrollpad';
+import 'antd/dist/antd.css';
+import { AutoComplete } from 'antd';
 
 const loginStyle = {
     width: "100%",
@@ -23,46 +24,92 @@ export default class Dashboard extends Component {
             })
         }) 
     } 
+
     state = {
-        data: []
+        user : false,
+        value : '',
+        dataSource : [],
+        data : [],
+        filter : []
     }
    
+    onSelect = (value) => {
+        const data = this.state.data;
+        let len = data.length;
+        for(var i=0; i<len; i++){
+          var string = data[i]["name"];
+          if(value===string){
+            this.setState({
+                user : true,
+                filter : data[i]
+              });
+          }}
+        
+      }
     
+      onSearch = searchText => {
+        const data = this.state.data;
+        let len = data.length;
+        var arr = [];
+        for(var i=0; i<len; i++){
+          var re = new RegExp(searchText, 'gi');
+          var string = data[i]["name"];
+          if(string.match(re)){
+            arr.push(string);
+          }}
+        this.setState({
+          dataSource: arr
+        });
+      };
+    
+      onChange = value => {
+        this.setState({ value });
+      };
+
     render() {
-                    
+        const { dataSource, value, data, user, filter } = this.state; 
+        let us = user;            
         return (
-            
-            <div style={loginStyle} className="white">
-            
-            <div className="card z-depth-0"><ScrollPad>
-                <div className="card-content center">
-                
-                    <Table striped bordered hover className='notification-list msgact notf2'>
-                        <thead>
-                        <tr>
+            <div style={loginStyle} className="white card z-depth-0 card-content center">
+                <div>
+                    <AutoComplete
+                    value={value}
+                    dataSource={dataSource}
+                    style={{ width: 200 }}
+                    onSelect={this.onSelect}
+                    onSearch={this.onSearch}
+                    onChange={this.onChange}
+                    placeholder="Filter Here By Name"
+                    />
+                </div>
+                <Table striped bordered hover className='notification-list msgact notf2'>
+                    <thead>
+                       <tr>
                             <td><h6>Id</h6></td>
                             <td><h6>Name</h6></td>
                             <td><h6>Salary</h6></td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        
-                        {this.state.data.map(item => (
-                        <React.Fragment key={item.id}>
+                    </thead>
+                    <tbody>
+                        {us && value!==''? 
                         <tr>
-                        <td> {item.id} </td>
-                        <td> {item.name} </td>
-                        <td> {item.salary} </td>
+                            <td> {filter.id} </td>
+                            <td> {filter.name} </td>
+                            <td> {filter.salary} </td>
                         </tr>
+                        :   data.map(item => (
+                        <React.Fragment key={item.id}>                        
+                            <tr>
+                                <td> {item.id} </td>
+                                <td> {item.name} </td>
+                                <td> {item.salary} </td>
+                            </tr>
                         </React.Fragment>
-                        ))}
-                        
-                        </tbody>
-                    </Table>
-                </div>
-                        </ScrollPad>
+                            ))
+                        }
+                    </tbody>
+                </Table>
             </div>
-        </div>
         )
     }
 }
